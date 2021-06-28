f1a14a99451f90bdba0b6906fcc94ffc5d8117b8
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-06-28 13:50:32 +0530

    EmployeeWageComputation with while has been added

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,16 +1,33 @@
 #! /bin/bash -x
 
 echo " welcome to EmployeeWageComputation"
-IS_PRESENT=1
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
 EMP_RATE_PER_HR=20
-empCheck=$((RANDOM%2))
-if [ $empCheck -eq $IS_PRESENT ]
-then
-        empHrs=8
+MAX_WORKING_DAYS=20
+day=1
+while  [ $day -le $MAX_WORKING_DAYS ]
+do
+        # Variable
+        ((day++))
+        empCheck=$((RANDOM%3))
+        #selection
+        case $empCheck in
+                $IS_PRESENT_FULL_TIME) empHrs=8
+                ;;
+                $IS_PRESENT_HALF_TIME) empHrs=4
+                ;;
+                *) empHrs=0
+                ;;
+        esac
+        totalWorkingHours=$(( totalWorkingHours + empHrs ))
         salary=$(( empHrs * EMP_RATE_PER_HR ))
-else
-        salary=0
-fi
+done
+totalSalary=$(( totalWorkingHours * EMP_RATE_PER_HR ))
+
+
+
+
 
 
 
