2c5845e460c6304bd5a354fd794e90cd89e801c9
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-06-28 14:00:08 +0530

    EmployeeWageComputation REfactor

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -5,11 +5,9 @@ IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 EMP_RATE_PER_HR=20
 MAX_WORKING_DAYS=20
-day=1
-while  [ $day -le $MAX_WORKING_DAYS ]
+for ((day=1;day<=$MAX_WORKING_DAYS;day++))
 do
         # Variable
-        ((day++))
         empCheck=$((RANDOM%3))
         #selection
         case $empCheck in
@@ -20,10 +18,10 @@ do
                 *) empHrs=0
                 ;;
         esac
-        totalWorkingHours=$(( totalWorkingHours + empHrs ))
         salary=$(( empHrs * EMP_RATE_PER_HR ))
 done
-totalSalary=$(( totalWorkingHours * EMP_RATE_PER_HR ))
+
+
 
 
 
